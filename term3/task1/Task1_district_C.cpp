b8a2101bfcf28b90d04a9eb3c47e5686781cd2e9
SAFUANlip <72254939+SAFUANlip@users.noreply.github.com>
2022-02-04 13:22:12 +0300

    Add files via upload

---
+++ term3/task1/Task1_district_C.cpp
@@ -0,0 +1,184 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <cmath>
+//какие рёбра соединяющие компоненты сильной связности прямая пересекает 
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+    Point(int _x, int _y) : x{ _x }, y{ _y }{};
+};
+
+vector<int> components(const vector<vector<int>>& g, function<int(int)> vorder = [](int i) {return i; })
+{
+    vector<int> c(g.size(), -1);
+    int ci = 0;
+    function<void(int)> dfs = [&](int v)
+    {
+        c[v] = ci;
+        for (int u : g[v])
+            if (c[u] == -1)
+                dfs(u);
+    };
+    
+    for (int i = 0; i < g.size(); ++i)
+    {
+        int v = vorder(i);
+        if (c[v] == -1)
+            dfs(v), ++ci;
+    }
+
+    return c;
+
+}
+
+
+
+void topological_sort_dfs(const vector<vector<int>>& g, vector<int>& res, vector<bool>& visited, int v)
+{
+    visited[v] = true;
+    for (int u : g[v])
+        if (!visited[u])
+            topological_sort_dfs(g, res, visited, u);
+    res.push_back(v);
+}
+
+
+vector<int> topological_sort(const vector<vector<int>>& g)
+{
+    vector<int> res(g.size());
+    vector<bool> visited(g.size());
+    for (int v = 0; v < g.size(); ++v)
+        if (!visited[v])
+            topological_sort_dfs(g, res, visited, v);
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+
+vector<vector<int>> invert(const vector<vector<int>>& g)
+{
+    vector<vector<int>> inverted(g.size());
+    for (int v = 0; v < g.size(); ++v)
+        for (int u : g[v])
+            inverted[u].push_back(v);
+    return inverted;
+}
+
+vector<int> kosar(const vector<vector<int>>& g)
+{
+    vector<int> out = topological_sort(invert(g));
+    return components(g, [&](int i) {return out[i]; });
+}
+
+
+int main()
+{
+    int n, m, x, y, from, to, k, b;
+    cin >> n >> m;
+    vector<Point> p;
+    p.reserve(n);
+    vector<vector<int>> vert(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> x >> y;
+        p.emplace_back(x,y);
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> from >> to;
+        if (from >= n || to >= n) {
+            cout << "you havent so many point or your count of point < 0";
+            return 0;
+        }
+        vert[from].push_back(to);
+    }
+    vector<int> comp = kosar(vert);
+    cin >> k >> b;
+    for (int i = 0; i < n; ++i) {
+        for (auto j : vert[i])
+            if (comp[j] != comp[i])
+                if ((p[i].y - k * p[i].x - b) * (p[j].y - k * p[j].x - b) < 0)
+                    cout << "point 1: " << p[i].x << p[i].y << " point 2: " << p[j].x << p[j].y <<  endl;
+    }
+    return 0;
+}
+/*
+7 8
+1 5
+3 10
+5 10
+5 6
+4 1
+3 -5
+-1 -4
+0 1
+1 2
+2 3
+3 0
+4 0
+4 5
+5 6
+6 4
+1 1
+*/
+//int main()
+//{
+//    int n,m;
+//    cin >> n >> m;
+//
+//    vector<vector<int>> out(n);
+//    vector<vector<int>> in(n);
+//
+//    for (int i = 0; i < m; ++i)
+//    {
+//        int first, second;
+//        cin >> first >> second;
+//        if (first != second) {
+//            //out[first-1].push_back(second-1);
+//            //in[second-1].push_back(first-1);
+//
+//            out[first].push_back(second);
+//            in[second].push_back(first);
+//        }
+//    }
+//
+//    int in_c = 0, out_c = 0;
+//
+//    pair<vector<int>, int> end = kosar(out);
+//    vector<int> res = end.first;
+//    int count = end.second;
+//    
+//    vector<vector<int>> comp(count);
+//    for (int v = 0; v < res.size(); ++v)
+//        comp[res[v]].push_back(v);
+//
+//    bool have_out = false, have_in = false;
+//
+//    for (int i = 0; i < comp.size(); ++i) {
+//        for (int j = 0; j < comp[i].size(); ++j) {
+//            if (comp[i].size() == n) {
+//                cout << 0;
+//                return 0;
+//            }
+//            for (int k = 0; k < out[comp[i][j]].size(); ++k)
+//                if (i != res[out[comp[i][j]][k]])
+//                    have_out = true;
+//            for (int k = 0; k < in[comp[i][j]].size(); ++k)
+//                if (i != res[in[comp[i][j]][k]])
+//                    have_in = true;
+//        }
+//        if (!have_out) 
+//            ++out_c;
+//        if (!have_in)
+//            ++in_c;
+//        have_out = false, have_in = false;
+//    }
+//
+//    cout << max(in_c, out_c);
+//
+//    return 0;
+//}
---
+++ term3/task1/Task1_secret_materials_B.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+//сделать только топологической сортировкой 
+using namespace std;
+
+//bool components(const vector<vector<int>>& g, function<int(int)> vorder = [](int i) {return i; })
+//{
+//    vector<int> c(g.size(), -1);
+//    int ci = 0;
+//    function<void(int)> dfs = [&](int v)
+//    {
+//        c[v] = ci;
+//        for (int u : g[v])
+//            if (c[u] == -1)
+//                dfs(u);
+//    };
+//    for (int i = 0; i < g.size(); ++i)
+//    {
+//        int v = vorder(i);
+//        if (c[v] == -1)
+//            dfs(v), ++ci;
+//    }
+//    vector<vector<int>> comp(ci);
+//    for (int v = 0; v < c.size(); ++v) {
+//        if (comp[c[v]].size() != 0)
+//            return false;
+//        else
+//            comp[c[v]].push_back(v);
+//    }
+//    return true;
+//}
+
+enum color
+{
+    white, gray, black
+};
+
+bool find_cycle_directed(const vector<vector<int>>& g)
+{
+    vector<color> colors(g.size(), white);
+    function<bool(int)> dfs = [&](int v) {
+        colors[v] = gray;
+        for (int u : g[v]) {
+            if (colors[u] == gray || (colors[u] == white && dfs(u)))
+                return true;
+        }
+        colors[v] = black;
+        return false;
+    };
+    for (int v = 0; v < g.size(); ++v)
+        if (colors[v] == white && dfs(v))
+            return true;
+    return false;
+}
+
+void topological_sort_dfs(const vector<vector<int>>& g, vector<int>& res, vector<bool>& visited, int v)
+{
+    visited[v] = true;
+    for (int u : g[v])
+        if (!visited[u])
+            topological_sort_dfs(g, res, visited, u);
+    res.push_back(v);
+}
+
+
+vector<int> topological_sort(const vector<vector<int>>& g)
+{
+    vector<int> res;
+    vector<bool> visited(g.size());
+    for (int v = 0; v < g.size(); ++v)
+        if (!visited[v])
+            topological_sort_dfs(g, res, visited, v);
+    reverse(res.begin(), res.end());
+    return res;
+}
+
+
+//vector<vector<int>> invert(const vector<vector<int>>& g)
+//{
+//    vector<vector<int>> inverted(g.size());
+//    for (int v = 0; v < g.size(); ++v)
+//        for (int u : g[v])
+//            inverted[u].push_back(v);
+//    return inverted;
+//}
+
+//bool kosar(const vector<vector<int>>& g)
+//{
+//    vector<int> out = topological_sort(invert(g));
+//    return components(g, [&](int i) {return out[i]; });
+//}
+
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> g(n);
+
+    for (int i = 0; i < m; ++i)
+    {
+        int first, second;
+        cin >> first >> second;
+        if (first == second) {
+            cout << "NO";
+            return 0;
+        }
+        g[first].push_back(second);
+    }
+ 
+    if (!find_cycle_directed(g)) {
+        cout << "YES"<<endl;
+        vector<int> go = topological_sort(g);
+        for (auto i : go)
+            cout << i << " ";
+    }
+    else
+        cout << "NO";
+    return 0;
+}
